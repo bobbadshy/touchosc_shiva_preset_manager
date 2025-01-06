e637d49a20abb5a5ab6f6546ffe23d557c85dcea
Sven Nielsen <post@svennielsen.de>
2025-01-06 19:38:18 +0100

    small edit

--- scripts/decompress.sh
+++ scripts/decompress.sh
@@ -5,6 +5,7 @@
 s="shiva_preset_manager.tosc"
 t="xml_export/shiva_preset_manager.xml"
 
-echo "Decompressing $s to $t .."
+echo "Decompressing $s to $t ..
+also formats the .xml a bit better to allow for better showing a git diff on it"
 
-pigz -c -d < "$s" > "$t"
+pigz -c -d < "$s" | sed -r 's#(<[a-z]+>)<#\1\n<#g' - > "$t"
