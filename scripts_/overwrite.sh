95dc971d21a5caa2b1c8bcf008d3007a28763879
Sven Nielsen <post@svennielsen.de>
2025-01-28 01:08:57 +0100

    remove old scripts

--- scripts_/overwrite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# read config
-. "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/config.sh"
-
-./decompress.sh || exit 1
-
-mv "$XML_SOURCE" "$XML_SOURCE.bak"
-cp -av "$XML_EXPORT" "$XML_SOURCE"
--- scripts_/start-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# read config
-. "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/config.sh"
-
-/opt/touchosc/TouchOSC "$TOSC_FINAL"
